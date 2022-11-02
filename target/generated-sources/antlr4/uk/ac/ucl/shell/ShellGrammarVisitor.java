4c9163a73158a89e189a5bd4307aa7f4d3d08e16
Lucas TItus <lucas@cybersole.io>
2022-11-02 14:33:35 +0000

    Generated missing targets + gitignore

---
+++ target/generated-sources/antlr4/uk/ac/ucl/shell/ShellGrammarVisitor.java
@@ -0,0 +1,25 @@
+// Generated from uk\ac\u005Cucl\shell\ShellGrammar.g4 by ANTLR 4.7
+package uk.ac.ucl.shell;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link ShellGrammarParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface ShellGrammarVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link ShellGrammarParser#command}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCommand(ShellGrammarParser.CommandContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ShellGrammarParser#atomicCommand}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAtomicCommand(ShellGrammarParser.AtomicCommandContext ctx);
+}
