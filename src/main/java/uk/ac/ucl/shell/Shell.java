77bee2b107e93905e5f9e7042b2fe4e6407ab91c
canrertugrul <91729868+canrertugrul@users.noreply.github.com>
2022-11-26 23:50:00 +0000

    ctrl + D stops user input

--- src/main/java/uk/ac/ucl/shell/Shell.java
+++ src/main/java/uk/ac/ucl/shell/Shell.java
@@ -6,6 +6,7 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
@@ -64,6 +65,9 @@ public class Shell {
                     try {
                         String cmdline = input.nextLine();
                         eval(cmdline, System.out);
+                    } catch (NoSuchElementException e) {
+                        input.close();
+                        break;
                     } catch (Exception e) {
                         System.out.println("COMP0010 shell: " + e.getMessage());
                     }
